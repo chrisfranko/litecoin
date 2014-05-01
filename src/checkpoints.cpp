958a752a2194b298bb6930daeb89df7a4154e295
Franko <chrisjfranko@gmail.com>
2014-05-01 09:35:28 -0400

    subsidy update
    Subsidy doubles the mining lifespan of the coin.
    
    update the readme
    
    also updated with a checkpoint

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -36,15 +36,16 @@ namespace Checkpoints
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
         (      0, uint256("0xee960feb663ca4231c851e0ef84205ec068a5efec38bcff39d83a6b637039449"))
-		(		2568, uint256("0x1d60287c5741ffb9bfc146ba3c03a1b0b4e1c1e936c69980b5efad8462452387"))
-		(       2923, uint256("0x45087cd52fe191763b27e03191bb18501ab9c73d1d6e5b9250ed406ebf95ef57"))
-        (       2934, uint256("0x61830c43e7ff6b9c604f7c4f02465af7536dd6886e5278932509b4e3eb06885c"))
-		(		19462, uint256("0x7d711707cf616e98351eff423de606912864827bca3a391f548f2b3b6fbfcfb0"))
+		(	2568, uint256("0x1d60287c5741ffb9bfc146ba3c03a1b0b4e1c1e936c69980b5efad8462452387"))
+		(   2923, uint256("0x45087cd52fe191763b27e03191bb18501ab9c73d1d6e5b9250ed406ebf95ef57"))
+        (   2934, uint256("0x61830c43e7ff6b9c604f7c4f02465af7536dd6886e5278932509b4e3eb06885c"))
+		(	9462, uint256("0x7d711707cf616e98351eff423de606912864827bca3a391f548f2b3b6fbfcfb0"))
+		(	70886, uint256("0xd4706846d5651b9d4a88f6a24f0b742ee0067e40f7883e6564c232c146505a08"))
         ;
     static const CCheckpointData data = {
         &mapCheckpoints,
-        1397398307, // * UNIX timestamp of last checkpoint block
-        35282,    // * total number of transactions between genesis and last checkpoint
+        1398951004, // * UNIX timestamp of last checkpoint block
+        122610,    // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
         80000.0     // * estimated number of transactions per day after checkpoint
     };
