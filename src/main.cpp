3e4485df65e26f8922225d9f16f757224dc9542a
snogcel <ledniknoj@gmail.com>
2014-05-01 07:18:38 -0600

    Block Halving Schedule
    Can't think of a better way to do this...

--- src/main.cpp
+++ src/main.cpp
@@ -1076,8 +1076,10 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
     int64 nSubsidy = 50 * COIN;
 
     // Subsidy is cut in half every 210000 blocks
-    nSubsidy >>= (nHeight / 210000);
-    if (nSubsidy < 3.125) nSubsidy = 3.125; // set minimum block reward
+    nSubsidy >>= (nHeight / 210000); // cut blocks every 210k
+    if (nHeight >= 420000) nSubsidy = 12.5 // non-linear...
+    if (nHeight >= 840000) nSubsidy = 6.25 // non-linear...
+    if (nHeight >= 1680000) nSubsidy = 3.125 // set minimum payout
 
     return nSubsidy + nFees;
 }
